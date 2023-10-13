0946c400adbe5c1a19c939c1dc6a54015d6ffe57
Babatunde Sanusi <swisskid95@gmail.com>
2023-10-13 17:28:27 +0100

    adjust margin on the description side of the dish details card

--- components/DishDetails.tsx
+++ components/DishDetails.tsx
@@ -22,7 +22,7 @@ export const DishDetails: React.FC<{
 
   return (
     <div className="px-4 pt-2">
-      <div className="shadow w-full h-[60%] bg-white grid grid-rows-3 max-h-[400px] grid-cols-2 grid-flow-col gap-4 overflow-hidden mt-10 rounded-lg">
+      <div className="shadow w-full h-[60%] bg-white grid grid-rows-3 max-h-[400px] grid-cols-2 grid-flow-col gap-4 overflow-hidden mt-5 md:mt-10 rounded-lg">
         <div className="row-span-3 col-span-1 w-full mx-auto">
           <Image
             src={currentDish.image}
@@ -35,8 +35,8 @@ export const DishDetails: React.FC<{
             loading="lazy"
           />
         </div>
-        <div className="row-span-3 col-span-1 relative mt-6 mr-4">
-          <h1 className="text-lg mt-[50px] md:text-4xl font-bold md:mb-4 md:mt-[75px]">
+        <div className="row-span-3 col-span-1 relative mt-2 md:mt-6 mr-4">
+          <h1 className="text-lg mt-[25px] md:text-4xl font-bold md:mb-4 md:mt-[75px]">
             {currentDish.name}
           </h1>
           <div
